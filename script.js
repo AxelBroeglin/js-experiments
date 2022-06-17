0255c4b3056eff2467e6a27a828a38d446a73b06
AxelBroeglin <axelbroeglin@gmail.com>
2022-06-17 23:30:48 +0100

    On click ready

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let myLibrary = [];
+
+let bookTitle = document.getElementById('book-title');
+let bookAuthor = document.getElementById('book-author');
+let bookPages = document.getElementById('book-pages');
+let bookRead = document.getElementById('book-read');
+let button = document.getElementById('button');
+
 function Book(title, author, pages, read){
     this.title = title
     this.author = author
@@ -9,6 +17,18 @@ function Book(title, author, pages, read){
 Book.prototype.info = function(){
     console.log(this.title + ' by ' + this.author + ', ' + this.pages + ', '+ this.read);
 }
+
+//Gathe rbook info on click
+button.addEventListener('click', e => {
+    console.log('le cul')
+    console.log(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+    }
+)
+
+function addBookToLibrary(){
+
+}
+
 
 const theHobbit = new Book('Hobbit', 'Tolkien', '213', 'not read yet')
 
