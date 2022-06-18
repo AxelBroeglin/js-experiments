b585576fb0090bfeb2cf2fed3b1ed7e68ff78b38
AxelBroeglin <axelbroeglin@gmail.com>
2022-06-18 17:46:57 +0100

    Delete buttons fuction (give unique id to divs)

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ let bookAuthor = document.getElementById('book-author');
 let bookPages = document.getElementById('book-pages');
 let bookRead = document.getElementById('book-read');
 let button = document.getElementById('button');
+let buttonDelete = document.getElementById('button-delete');
 
 //Function Book
 function Book(title, author, pages, read){
@@ -42,19 +43,6 @@ Book.prototype.info = function(){
     console.log(this.title + ' by ' + this.author + ', ' + this.pages + ', '+ this.read);
 }
 
-
-
-main.innerHTML = '';
-myLibrary.forEach(book => {
-    console.log(myLibrary);
-    const bookCard = document.createElement('div');
-    bookCard.classList.add('book');
-    bookCard.innerHTML = '<ul><li>' + book.title + '</li><li>'+ book.author +'</li><li>'+ book.pages +'</li><li>'+ book.read +'</li>'
-    main.appendChild(bookCard);
-})
-
-
-
 //Defines the function to add to library
 function addBookToLibrary(){
     //Gather book info on click
@@ -63,7 +51,31 @@ function addBookToLibrary(){
     console.log(myLibrary);
 }
 
+function newBook(){
+    main.innerHTML = '';
+
+    myLibrary.forEach(book =>{
+        let bookCard = document.createElement('div');
+        myLibraryLength = myLibrary.length;
+        for (let i=0; i < myLibraryLength; i++){
+            console.log(i);
+            bookCard.setAttribute = ('id', 'i');
+        }
+        bookCard.innerHTML = `<ul><li>` + book.title + `</li><li>` + book.author + `</li><li>` + book.pages + `</li><li>` + book.read +  `</li><li><button id="button-delete">X</button>`
+        main.appendChild(bookCard);
+    })
+}
+newBook()
+
+
 //Click on the button triggers the function
 button.addEventListener('click', e => {
-    addBookToLibrary()
+    addBookToLibrary();
+    newBook();
 })
+
+// //Button to delete book
+// buttonDelete.addEventListener('click', e => {
+//     e.stopPropagation();
+
+// })
