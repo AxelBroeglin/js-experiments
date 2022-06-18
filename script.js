aefb1e6254f360ae1c59dac5ae2bcd8f4e744908
AxelBroeglin <axelbroeglin@gmail.com>
2022-06-18 16:41:36 +0100

    forEach loop works

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-    //Array
+//Array
 let myLibrary = [
     {
-        'Title':'Hobbit', 
-        'Author':'Tolkien', 
-        'Pages':'213', 
-        'Read':'yes'
+        'title':'Hobbit', 
+        'author':'Tolkien', 
+        'pages':'213', 
+        'read':'yes'
     },
     {
-        'Title':'Dune', 
-        'Author':'Herbert', 
-        'Pages':'603', 
-        'Read':'yes'
+        'title':'Dune', 
+        'author':'Herbert', 
+        'pages':'603', 
+        'read':'yes'
     },
     {
-        'Title':'Surveiller et punir', 
-        'Author':'Foucault', 
-        'Pages':'213', 
-        'Read':'no'
+        'title':'Surveiller et punir', 
+        'author':'Foucault', 
+        'pages':'213', 
+        'read':'no'
     }
 ];
 
-    //Variables
+//Variables
+let main = document.getElementById('main');
 let bookTitle = document.getElementById('book-title');
 let bookAuthor = document.getElementById('book-author');
 let bookPages = document.getElementById('book-pages');
 let bookRead = document.getElementById('book-read');
 let button = document.getElementById('button');
 
-    //Function Book
+//Function Book
 function Book(title, author, pages, read){
     this.title = title
     this.author = author
@@ -36,23 +37,33 @@ function Book(title, author, pages, read){
     this.info
     }
 
-    //Prototype to log a Book
+//Prototype to log a Book
 Book.prototype.info = function(){
     console.log(this.title + ' by ' + this.author + ', ' + this.pages + ', '+ this.read);
 }
 
 
 
+main.innerHTML = '';
+myLibrary.forEach(book => {
+    console.log(myLibrary);
+    const bookCard = document.createElement('div');
+    bookCard.classList.add('book');
+    bookCard.innerHTML = '<ul><li>' + book.title + '</li><li>'+ book.author +'</li><li>'+ book.pages +'</li><li>'+ book.read +'</li>'
+    main.appendChild(bookCard);
+})
 
 
+
+//Defines the function to add to library
 function addBookToLibrary(){
-//Gather book info on click
+    //Gather book info on click
     let book = {'title':bookTitle.value, 'author':bookAuthor.value, 'pages':bookPages.value, 'read':bookRead.value}
     myLibrary.unshift(book);
     console.log(myLibrary);
 }
 
-    //Click on the button triggers the function
+//Click on the button triggers the function
 button.addEventListener('click', e => {
     addBookToLibrary()
 })
