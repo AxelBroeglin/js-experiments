77231f380f0fbcb112e720501a76276161614006
Axel Broeglin <axelbroeglin@gmail.com>
2022-06-20 20:41:49 +0100

    Players factory

--- tictactoe.js
+++ tictactoe.js
@@ -28,15 +28,27 @@ let boxArray = [
         
     }
 ];
-console.log(boxArray.length)
 
-function createBoard () {
-    boxArray.forEach(box => {
-    console.log(boxArray.length)
-    const boxDiv = document.createElement('div');
-    boxDiv.classList.add('box');
-    // boxDiv.addEventListener('click', fillBox);
-    document.querySelector('#box-container').appendChild(boxDiv);
-    });
-}
-createBoard ();
+
+//BOARD
+const board = (() => {
+    const box = boxArray.forEach(box => {
+        const boxDiv = document.createElement('div');
+        boxDiv.classList.add('box');
+        // boxDiv.addEventListener('click', fillBox);
+        document.querySelector('#box-container').appendChild(boxDiv);
+        });
+    return {box};
+})();
+
+
+
+//PLAYERS
+const playersFactory = (name, sign) => {
+    return { name, sign};
+};
+  
+const player1 = playersFactory('Player 1', 'X');
+const player2 = playersFactory('Player 2', 'O');
+  
+console.log(player1.name);
