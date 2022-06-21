8e232d1d8efef8c2d535d27c2aa9bce465ba1645
Axel Broeglin <axelbroeglin@gmail.com>
2022-06-21 18:22:45 +0100

    Working on fillBox function

--- tictactoe.js
+++ tictactoe.js
@@ -1,59 +1,68 @@
 let boxArray = [
     {
         'box' : 'box',
-        'sign' : ''
+        'sign' : 'O'
     },    
     {
         'box' : 'box',
-        'sign' : ''
+        'sign' : 'X'
     },    
     {
         'box' : 'box',
-        'sign' : ''
+        'sign' : ' '
     },    
     {
         'box' : 'box',
-        'sign' : ''    
+        'sign' : ' '    
     },    
     {
         'box' : 'box',
-        'sign' : ''    
+        'sign' : ' '    
     },    
     {
         'box' : 'box',
-        'sign' : ''    
+        'sign' : ' '    
     },    
     {
         'box' : 'box',
-        'sign' : ''    
+        'sign' : ' '    
     },    
     {
         'box' : 'box',
-        'sign' : ''    
+        'sign' : ' '    
     },    
     {
         'box' : 'box',
-        'sign' : ''        
+        'sign' : ' '        
     }
 ];
 
+const box = document.querySelector('.box')
 
 //BOARD
 const board = (() => {
     const box = boxArray.forEach(box => {
         const boxDiv = document.createElement('div');
         boxDiv.classList.add('box');
-        // boxDiv.addEventListener('click', fillBox);
+        boxDiv.addEventListener('click', fillBox);
         document.querySelector('#box-container').appendChild(boxDiv);
         });
     return {box};
 })();
 
+function fillBox(){
+    console.log(boxArray.sign)
+    if(boxArray.sign != 'X' || 'O'){
+        console.log('hello');
+    }else{
+        console.log('pas hello')
+    }
+}
 
 
 //PLAYERS
 const playersFactory = (name, sign) => {
-    return { name, sign};
+    return {name, sign};
 };
   
 const player1 = playersFactory('Player 1', 'X');
@@ -62,6 +71,7 @@ const player2 = playersFactory('Player 2', 'O');
 
 
 //GAMEFLOW
+//Only empty boxes can be clicked (lose <a> and cursor)
 //Must contain add.eventlistener for each box, check if box occupied by checking sign in array, if not occupied, charge sign in array.
 //checks if 3 in a row
 //other player (count++, even/odd number to define who plays ?)
