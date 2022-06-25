1876161583884a3fef2b091878c7f7f47a9e0566
AxelBroeglin <axelbroeglin@gmail.com>
2022-06-25 19:33:47 +0100

    Functions

---
+++ rockpaperscissors.js
@@ -0,0 +1,16 @@
+const body = document.getElementById('body');
+const button = document.querySelectorAll('button');
+
+function computerChoice() {
+
+}
+
+function playerSelection() {
+
+}
+
+function gameFlow() {
+
+}
+
+gameFlow();
