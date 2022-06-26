1487ba9c0f360c83902349ee6faec96b7d382eca
Axel Broeglin <axelbroeglin@gmail.com>
2022-06-26 09:48:47 +0100

    playRound() works

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,5 @@
-const playerSelection = 'rock';
-const computerSelection = computerPlay();
+let playerSelection = 'rock';
+let computerSelection = computerPlay();
 let result
 console.log(playerSelection, computerSelection)
 
@@ -8,7 +8,7 @@ function computerPlay(){
     switch (computerChoice) {
         case 1:
             computerChoice = 'rock';
-          break;
+          break;  
 
         case 2:
             computerChoice = 'paper';
@@ -20,17 +20,31 @@ function computerPlay(){
     return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection){
-    //playerSelection and  computerSelection are undefined in this function for some reason
 
+function playRound(playerSelection, computerSelection){
     console.log(playerSelection, computerSelection)
     if (computerSelection === playerSelection) {
-        result = 'its a draw!'
-      }
-    else{
-        result = 'else';
+      result = 'its a draw!'
     }
-    console.log(result)
+    if (computerSelection === 'rock' && playerSelection === "paper") {
+      result = 'you win!'
+    }
+    if (computerSelection === 'rock' && playerSelection === "scissors") {
+      result = 'you lost!'
+    }
+    if (computerSelection === 'paper' && playerSelection === "scissors") {
+      result = 'you win!'
+    }
+    if (computerSelection === 'paper' && playerSelection === "rock") {
+      result = 'you lose!'
+    }
+    if (computerSelection === 'scissors' && playerSelection === "rock") {
+      result = 'you win!'
+    }
+    if (computerSelection === 'scissors' && playerSelection === "paper") {
+      result = 'you lose!'
+    }
+    return result
 }
 
-playRound();
+console.log(playRound(playerSelection, computerSelection))
